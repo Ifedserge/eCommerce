b516cc2322183356dfab8123ef627feb51f3b9a5
Kirill Kostin <79797389+konmedoed@users.noreply.github.com>
2024-05-12 21:32:16 +0300

    refactor: add functions for creating tags (#73)

---
+++ src/scripts/components/types/enums.ts
@@ -0,0 +1,29 @@
+export enum InputType {
+  text = 'text',
+  submit = 'submit',
+  button = 'button',
+  password = 'password',
+  radio = 'radio',
+  checkbox = 'checkbox',
+  reset = 'reset',
+}
+
+export enum HeadingType {
+  h1 = 'h1',
+  h2 = 'h2',
+  h3 = 'h3',
+  h4 = 'h4',
+  h5 = 'h5',
+  h6 = 'h6',
+}
+
+export enum BlockType {
+  header = 'header',
+  footer = 'footer',
+  main = 'main',
+  aside = 'aside',
+  nav = 'nav',
+  article = 'article',
+  section = 'section',
+  div = 'div',
+}
---
+++ src/scripts/services/utilities/tags.ts
@@ -0,0 +1,73 @@
+import { TagAttributes } from '../../components/types/interfaces';
+import { BlockType, InputType, HeadingType } from '../../components/types/enums';
+
+export const createBlock = (type: BlockType, className: string[]) => {
+  const elem = document.createElement(type);
+  className.forEach((item) => elem.classList.add(item));
+  return elem;
+};
+
+export const createInput = (
+  inputType: InputType,
+  className: string[],
+  ...attributes: TagAttributes[]
+) => {
+  const elem = document.createElement('input');
+  elem.setAttribute('type', inputType);
+  className.forEach((item) => elem.classList.add(item));
+  if (attributes) {
+    attributes.forEach((item) => elem.setAttribute(item.name, item.value ? item.value : ''));
+  }
+  return elem;
+};
+
+export const createForm = (className: string[], ...attributes: TagAttributes[]) => {
+  const elem = document.createElement('form');
+  className.forEach((item) => elem.classList.add(item));
+  if (attributes) {
+    attributes.forEach((item) => elem.setAttribute(item.name, item.value ? item.value : ''));
+  }
+  return elem;
+};
+
+export const createLabel = (className: string[], text?: string, ...attributes: TagAttributes[]) => {
+  const elem = document.createElement('label');
+  className.forEach((item) => elem.classList.add(item));
+  if (text) elem.innerText = text;
+  if (attributes) {
+    attributes.forEach((item) => elem.setAttribute(item.name, item.value ? item.value : ''));
+  }
+  return elem;
+};
+
+export const createButton = (className: string[], text: string, ...attributes: TagAttributes[]) => {
+  const elem = document.createElement('button');
+  className.forEach((item) => elem.classList.add(item));
+  elem.innerText = text;
+  if (attributes) {
+    attributes.forEach((item) => elem.setAttribute(item.name, item.value ? item.value : ''));
+  }
+  return elem;
+};
+
+export const createP = (className: string[], text?: string) => {
+  const elem = document.createElement('p');
+  className.forEach((item) => elem.classList.add(item));
+  if (text) elem.innerText = text;
+  return elem;
+};
+
+export const createHeading = (className: string[], text: string, tag: HeadingType) => {
+  const elem = document.createElement(tag);
+  className.forEach((item) => elem.classList.add(item));
+  elem.innerText = text;
+  return elem;
+};
+
+export const createLink = (className: string[], address: string, name: string) => {
+  const elem = document.createElement('a');
+  className.forEach((item) => elem.classList.add(item));
+  elem.setAttribute('href', address);
+  elem.append(name);
+  return elem;
+};
