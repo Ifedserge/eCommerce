70adaf05ba0b1d5ed57a3666d33df15fc20c5576
siviavio4ka <siviavio4ka@gmail.com>
2024-05-14 00:48:20 +0300

    feat: add method for rendering login page

---
+++ src/scripts/components/views/login/login.ts
@@ -0,0 +1,62 @@
+import { onInputEmailChange, onInputPasswordChange } from '../../../services/login/loginButtons';
+import {
+  createBlock, createButton, createForm, createInput, createLabel,
+} from '../../../services/utilities/tags';
+import { BlockType, InputType } from '../../types/enums';
+
+export class Login {
+  static render() {
+    const { body } = document;
+    body.innerHTML = '';
+
+    this.render.toString();
+    const loginForm = createForm(['login-form']);
+
+    const fieldset = document.createElement('fieldset');
+    fieldset.classList.add('auth-fieldset');
+
+    const legend = document.createElement('legend');
+    legend.textContent = 'Login';
+
+    const emailContainer = createBlock(BlockType.div, ['form-group']);
+
+    const emailLabel = createLabel(['form-label'], 'Email');
+    const emailInput = createInput(InputType.text, ['form-control']);
+    emailInput.setAttribute('placeholder', 'Enter email');
+    emailInput.addEventListener('keypress', onInputEmailChange);
+
+    emailContainer.appendChild(emailLabel);
+    emailContainer.appendChild(emailInput);
+
+    const passwordContainer = createBlock(BlockType.div, ['form-group']);
+
+    const passwordLabel = createLabel(['form-label'], 'Password');
+    const passwordInput = createInput(InputType.password, ['form-control']);
+    passwordInput.setAttribute('placeholder', 'Enter password');
+    passwordInput.addEventListener('keyup', onInputPasswordChange);
+
+    const eye = document.createElement('i');
+    eye.textContent = '👁️‍🗨️';
+    eye.addEventListener('click', () => {
+      if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+      } else {
+        passwordInput.type = 'password';
+      }
+    });
+
+    passwordContainer.appendChild(passwordLabel);
+    passwordContainer.appendChild(passwordInput);
+    passwordContainer.appendChild(eye);
+
+    fieldset.appendChild(legend);
+    fieldset.appendChild(emailContainer);
+    fieldset.appendChild(passwordContainer);
+
+    const submitButton = createButton(['btn', 'btn-primary'], 'Login');
+
+    loginForm.appendChild(fieldset);
+    loginForm.appendChild(submitButton);
+    body.appendChild(loginForm);
+  }
+}
