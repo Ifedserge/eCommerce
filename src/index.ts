53d0f219e4866154f10250bcfa44716481115b22
Kirill Kostin <konmedoed@gmail.com>
2024-05-16 01:40:00 +0300

    refactor: add reseting style for links and revert structure

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import './sass/main.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { App } from './scripts/components/views/app/app';
+import { App } from './scripts/components/app/app';
 
 const app = new App();
 app.start();
