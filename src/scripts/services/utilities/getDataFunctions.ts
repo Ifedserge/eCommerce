bdbb7ab0be493ca1063d9e4358640957cdb85d8b
Kirill Kostin <79797389+konmedoed@users.noreply.github.com>
2024-06-06 12:23:07 +0300

    feat: implement cards sorting (#133)
    * feat: implement cards sorting
    
    * fix: resolve an error with sorting all products

--- src/scripts/services/utilities/getDataFunctions.ts
+++ src/scripts/services/utilities/getDataFunctions.ts
@@ -1,5 +1,5 @@
 import { Category } from '@commercetools/platform-sdk';
-import { NotificationType } from '../../components/types/enums';
+import { NotificationType, SortType, SortingValue } from '../../components/types/enums';
 import { IProductAllData, IProductData } from '../../components/types/interfaces';
 import { NotificationService } from './notification';
 import { Api } from '../api';
@@ -13,7 +13,6 @@ export function getProducts(
     .products()
     .get({
       queryArgs: {
-        offset: 1,
         limit: 10,
       },
     })
@@ -75,4 +74,62 @@ export function getCategories(callback: (data: Category[]) => void): void {
         NotificationType.error
       );
     });
+}
+
+export function sortCards(
+  value: SortingValue,
+  id: string,
+  sortingType: SortType,
+  callBack: (data: IProductData | IProductAllData) => HTMLElement,
+  block: HTMLElement
+): void | IProductData[] | IProductAllData[] {
+  if (id === 'none' || id === '') {
+    apiAnonRoot
+      .productProjections()
+      .search()
+      .get({
+        queryArgs: {
+          filter: `categories.id:"1309b310-7a8e-4651-b120-91b5f3e84e08"`,
+          sort: `${value} ${sortingType}`,
+          limit: 10,
+        },
+      })
+      .execute()
+      .then((response) => {
+        response.body.results.forEach((item) => {
+          const data = item as unknown as IProductAllData;
+          block.append(callBack(data));
+        });
+      })
+      .catch(() => {
+        NotificationService.showNotification(
+          'Something happened. Please, go to the main page...',
+          NotificationType.error
+        );
+      });
+  } else {
+    apiAnonRoot
+      .productProjections()
+      .search()
+      .get({
+        queryArgs: {
+          filter: `categories.id:"${id}"`,
+          sort: `${value} ${sortingType}`,
+          limit: 10,
+        },
+      })
+      .execute()
+      .then((response) => {
+        response.body.results.forEach((item) => {
+          const data = item as unknown as IProductData;
+          block.append(callBack(data));
+        });
+      })
+      .catch(() => {
+        NotificationService.showNotification(
+          'Something happened. Please, go to the main page...',
+          NotificationType.error
+        );
+      });
+  }
 }
